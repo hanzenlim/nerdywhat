56e5ff85ca291b65b6ddc6b98699dd690b753bb4
mamonluk <mamonluk7@gmail.com>
2011-09-27 23:03:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yuhu::Application.config.secret_token = '78865caba906e17adfb4162e26e0c5ebb07187495c0b39eafb0babca01b520e1056e18d669dcef7729dff73d9a786d6a8e14459684416c08d1ab4d7746da2376'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Yuhu::Application.config.session_store :cookie_store, :key => '_yuhu_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Yuhu::Application.config.session_store :active_record_store
