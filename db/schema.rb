430f9f5fa74ce94f5d9346ff47f84acae1189754
mamonluk <mamonluk7@gmail.com>
2011-09-30 00:54:55 -0700

    sfsdf

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(:version => 20110930051752) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.string   "link"
+    t.text     "link"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -4,8 +4,7 @@ desc "This task is called by the Heroku cron add-on"
 task :cron => :environment do
  
    puts "hello"
-   user=User.new(:name=>"hello there", :link=> "www.gogole.com")
-   user.save
+   User.sayHello
    
    
 end
